238f99b10026ff4acf2652b167325643871eb321
Adil <81522631+IceFroggit@users.noreply.github.com>
2023-02-18 02:25:52 +0300

    Add files via upload
    pre-final version

---
+++ compGrahp_LR1/src/main/java/com/example/compgrahp_lr1/MyLine.java
@@ -0,0 +1,19 @@
+package com.example.compgrahp_lr1;
+
+import javafx.scene.shape.Line;
+
+public class MyLine {
+    double startX;
+    double startY;
+    double endX;
+    double endY;
+    Line line;
+
+    public MyLine(double startX, double startY, double endX, double endY) {
+        this.startX = startX;
+        this.startY = startY;
+        this.endX = endX;
+        this.endY = endY;
+        line = new Line(400 + startX, 400 - startY, 400 + endX, 400 - endY);
+    }
+}
